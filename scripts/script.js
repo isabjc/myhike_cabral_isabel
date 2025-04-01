6558a027f6fb4d819e23503df21e13c242a328a1
Isabel J Cabral <isabeljcabral@gmail.com>
2025-04-01 08:54:49 -0700

    added a jumbotron for the login page

--- scripts/script.js
+++ scripts/script.js
@@ -6,4 +6,16 @@
 function sayHello() {
     //do something
 }
-//sayHello();    //invoke function
+sayHello();    //invoke function
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
